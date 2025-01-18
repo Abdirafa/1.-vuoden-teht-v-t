024c751f68aa708e6fad4a28c89fc4342ecc602d
Abdirafa <Diidaad03@gmail.com>
2025-01-18 23:13:57 +0200

    teht 2

---
+++ .venv/Moduuli 2.py
@@ -0,0 +1,27 @@
+1.
+
+print("Hei, Abdirahman! Mukava tavata sinut!")
+
+2.
+
+sade = 5
+pinta_ala = 3.14159 * sade**2
+print(f"Ympyrän pinta-ala säteellä {sade} on {pinta_ala:.2f}")
+
+3.
+
+kanta = 7
+korkeus = 4
+piiri = 2 * (kanta + korkeus)
+pinta_ala = kanta * korkeus
+print(f"Suorakulmion piiri on {piiri} ja pinta-ala on {pinta_ala}")
+
+4.
+
+luku1 = 3
+luku2 = 7
+luku3 = 10
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = summa / 3
+print(f"Lukujen {luku1}, {luku2} ja {luku3} summa on {summa}, tulo on {tulo} ja keskiarvo on {keskiarvo:.2f}")
